e47f4f47d01bd3e3ad19551bfd33bbefbe1b22b9
jaywcjlove <398188662@qq.com>
2019-08-20 15:47:39 +0800

    Escape the HTML tags.

--- src/components/Markdown/allowNode.tsx
+++ src/components/Markdown/allowNode.tsx
@@ -5,7 +5,8 @@ export default function allowNode(node: any, index: number, parent: NodeType) {
   const nodeany: MarkdownAbstractSyntaxTree = node;
   if (nodeany.type === 'html') {
     // filter style
-    node.value = node.value.replace(/<style/g, '&lt;style').replace(/<\/style>/g, '&lt;style&gt;');
+    node.value = node.value.replace(/<(style|script|link|input|form)(.*)>/g, '&lt;$1$2&gt')
+      .replace(/<\/(.*)(style|script|link|input|form)(.*)>/g, '&lt;/$1$2$3&gt')
   }
   return node;
 }
