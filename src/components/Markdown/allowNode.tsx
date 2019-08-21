3336dad2a19f0076e5d79fd2a8de025891390ddf
jaywcjlove <398188662@qq.com>
2019-08-21 15:08:13 +0800

    Fix the escaping tag issue.

--- src/components/Markdown/allowNode.tsx
+++ src/components/Markdown/allowNode.tsx
@@ -5,8 +5,9 @@ export default function allowNode(node: any, index: number, parent: NodeType) {
   const nodeany: MarkdownAbstractSyntaxTree = node;
   if (nodeany.type === 'html') {
     // filter style
-    node.value = node.value.replace(/<(style|script|link|input|form)(.*)>/g, '&lt;$1$2&gt')
-      .replace(/<\/(.*)(style|script|link|input|form)(.*)>/g, '&lt;/$1$2$3&gt')
+    node.value = node.value.replace(/<((style|script|link|input|form)|\/(style|script|link|input|form))(\s?[^\>]*\>)/g, (a: string) => {
+      return a.replace(/[<>]/g, (e: string) => (({ '<': '&lt;', '>': '&gt;' } as { [key: string]: string })[e]))
+    });
   }
   return true;
 }
