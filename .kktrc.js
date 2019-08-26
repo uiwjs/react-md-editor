1770056b58b971a0c0257875a804ef548f66a82a
jaywcjlove <398188662@qq.com>
2019-08-26 13:08:17 +0800

    Modify publicPath for kktrc config.

--- .kktrc.js
+++ .kktrc.js
@@ -27,7 +27,7 @@ module.exports = {
     if (env === 'prod') {
       // Webpack configuration changed in production mode
       conf.entry[0] = path.resolve(process.cwd(), 'website');
-      conf.output.publicPath = '';
+      conf.output.publicPath = './';
       conf.output.path = path.resolve(__dirname, 'doc');
     } else {
       conf.entry[1] = path.resolve(process.cwd(), 'website', 'index.js');
