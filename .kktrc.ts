d0fa1cf6c269adebc33da11da2f58ffe6d839738
jaywcjlove <398188662@qq.com>
2020-04-02 10:12:01 +0800

    chore(deps): update dependency kkt to v5.6.1

--- .kktrc.ts
+++ .kktrc.ts
@@ -1,16 +1,20 @@
+import { OptionConf, LoaderOneOf, ModuleScopePluginOpts } from 'kkt';
+import webpack from 'webpack';
 import path from 'path';
 
-export const moduleScopePluginOpts = [
+type Webpack = typeof webpack;
+
+export const moduleScopePluginOpts: ModuleScopePluginOpts = [
   path.resolve(process.cwd(), 'README.md'),
 ];
 
-export const loaderOneOf = [
+export const loaderOneOf: LoaderOneOf = [
   [require.resolve('@kkt/loader-less'), {}],
 ]
 
-export default (conf, opts, webpack) => {
+export default (conf: webpack.Configuration, options: OptionConf, webpack: Webpack) => {
   const pkg = require(path.resolve(process.cwd(), 'package.json'));
-  conf.module.rules.map((item) => {
+  conf.module!.rules.map((item) => {
     if (item.oneOf) {
       item.oneOf.unshift({
         test: /\.md$/,
@@ -20,7 +24,7 @@ export default (conf, opts, webpack) => {
     return item;
   });
   // 获取版本
-  conf.plugins.push(
+  conf.plugins!.push(
     new webpack.DefinePlugin({
       VERSION: JSON.stringify(pkg.version),
     })
