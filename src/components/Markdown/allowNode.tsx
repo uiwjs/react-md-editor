2534a635480fd6fed95d00a5ccc9e0fe073e5a0f
jaywcjlove <398188662@qq.com>
2020-04-15 17:27:49 +0800

    style: Modify markdown styles.

--- src/components/Markdown/allowNode.tsx
+++ src/components/Markdown/allowNode.tsx
@@ -4,7 +4,7 @@ export default function allowNode(node: any, index: number, parent: NodeType) {
   const nodeany: MarkdownAbstractSyntaxTree = node;
   if (nodeany.type === 'html') {
     // filter style
-    node.value = node.value.replace(/<((style|script|link|input|form)|\/(style|script|link|input|form))(\s?[^\>]*\>)/gi, (a: string) => {
+    node.value = node.value.replace(/<((style|script|link|input|form)|\/(style|script|link|input|form))(\s?[^>]*>)/gi, (a: string) => {
       return a.replace(/[<>]/g, (e: string) => (({ '<': '&lt;', '>': '&gt;' } as { [key: string]: string })[e]))
     });
   }
