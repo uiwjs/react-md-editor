084210b7e70af4dde2eaa366b27db51c744c2844
jaywcjlove <398188662@qq.com>
2021-05-20 18:01:42 +0800

    type: Fix export type errors. #172

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import MDEditor from './Editor';
 import * as commands from './commands';
 import * as MarkdownUtil from './utils/markdownUtils';
 
-export type { ICommand, CommandOrchestrator, TextRange, TextState, TextApi } from './commands';
-export type { TextSection } from './utils/markdownUtils';
-
+export * from './commands';
+export * from './utils/markdownUtils';
 export * from './Editor';
 
 export { MarkdownUtil, commands };
