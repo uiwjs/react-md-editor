54c2e362198bdac96fdff288804ae354809186db
Stephen Eckels <stevemk14ebr@gmail.com>
2021-06-25 14:51:32 -0400

    Abstract onChange to general text

--- src/components/TextArea/Textarea.tsx
+++ src/components/TextArea/Textarea.tsx
@@ -8,7 +8,7 @@ import { MDEditorProps } from '../../Editor';
 import './index.less';
 
 type RenderTextareaHandle = {
-  handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  handleChange: (text: string) => void;
 };
 
 export interface ReRenderTextAreaProps {
@@ -58,9 +58,9 @@ export default function Textarea(props: TextAreaProps & ReRenderTextAreaProps) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    dispatch && dispatch({ markdown: e.target.value });
-    onChange && onChange(e.target.value);
+  const handleChange = (text: string) => {
+    dispatch && dispatch({ markdown: text });
+    onChange && onChange(text);
   };
 
   if (renderTextarea) {
@@ -87,7 +87,7 @@ export default function Textarea(props: TextAreaProps & ReRenderTextAreaProps) {
       ref={textRef}
       className={`${prefixCls}-text-input ${other.className ? other.className : ''}`}
       value={markdown}
-      onChange={handleChange}
+      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleChange(e.target.value)}
     />
   );
 }
