4b9fda6317d253dc147083466bcd2bfa51befce8
Phillip Burch <phillip.burch@live.com>
2021-12-03 09:14:00 -0600

    Make sure title markers is inserted at beginning of line

--- src/commands/title1.tsx
+++ src/commands/title1.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { insertAtLineStart } from '../utils/InsertTextAtPosition';
 import { ICommand, TextState, TextAreaTextApi } from './';
 
 export const title1: ICommand = {
@@ -8,10 +9,6 @@ export const title1: ICommand = {
   buttonProps: { 'aria-label': 'Insert title 1', title: 'Insert title 1' },
   icon: <div style={{ fontSize: 18, textAlign: 'left' }}>Title 1</div>,
   execute: (state: TextState, api: TextAreaTextApi) => {
-    let modifyText = `# ${state.selectedText}\n`;
-    if (!state.selectedText) {
-      modifyText = `# `;
-    }
-    api.replaceSelection(modifyText);
+    insertAtLineStart('# ', state.selection.start, api.textArea);
   },
 };
--- src/commands/title3.tsx
+++ src/commands/title3.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { insertAtLineStart } from '../utils/InsertTextAtPosition';
 import { ICommand, TextState, TextAreaTextApi } from './';
 
 export const title3: ICommand = {
@@ -8,10 +9,6 @@ export const title3: ICommand = {
   buttonProps: { 'aria-label': 'Insert title3', title: 'Insert title 3' },
   icon: <div style={{ fontSize: 15, textAlign: 'left' }}>Title 3</div>,
   execute: (state: TextState, api: TextAreaTextApi) => {
-    let modifyText = `### ${state.selectedText}\n`;
-    if (!state.selectedText) {
-      modifyText = `### `;
-    }
-    api.replaceSelection(modifyText);
+    insertAtLineStart('### ', state.selection.start, api.textArea);
   },
 };
