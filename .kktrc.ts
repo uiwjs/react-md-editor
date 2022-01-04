d9d4b73ce9e5464023d114e8f536a167ce584b4a
jaywcjlove <398188662@qq.com>
2022-01-04 09:37:03 +0800

    chore(deps): update dependency kkt to v7
    Upgrade react-scripts to v5, Support Webpack 5.x
    https://github.com/kktjs/kkt/issues/198

--- .kktrc.ts
+++ .kktrc.ts
@@ -44,34 +44,50 @@ export default (conf: Configuration, env: 'production' | 'development', options:
         VERSION: JSON.stringify(pkg.version),
       }),
     );
-    conf.optimization = {
-      ...conf.optimization,
-      splitChunks: {
-        cacheGroups: {
-          reactvendor: {
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            name: 'react-vendor',
-            chunks: 'all',
-          },
-          katex: {
-            test: /[\\/]node_modules[\\/](katex)[\\/]/,
-            name: 'katex-vendor',
-            chunks: 'all',
-          },
-          micromark: {
-            test: /[\\/]node_modules[\\/](micromark)[\\/]/,
-            name: 'micromark-vendor',
-            chunks: 'all',
-          },
-          prismjs: {
-            test: /[\\/]node_modules[\\/](prismjs)[\\/]/,
-            name: 'prismjs-vendor',
-            chunks: 'async',
+    if (env === 'production') {
+      conf.optimization = {
+        ...conf.optimization,
+        splitChunks: {
+          chunks: 'all', // async 对异步引入的代码分割 initial 对同步引入代码分割 all 对同步异步引入的分割都开启
+          minSize: 30000, // 字节 引入的文件大于30kb才进行分割
+          minChunks: 1, // 模块至少使用次数
+          maxAsyncRequests: 30, // 同时加载的模块数量最多是_个，只分割出同时引入的前_个文件（按需加载模块）
+          maxInitialRequests: 25, // 首页加载的时候引入的文件最多 _ 个（加载初始页面）
+          automaticNameDelimiter: '~', // 缓存组和生成文件名称之间的连接符
+          cacheGroups: {
+            reactvendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+              name: 'react-vendor',
+              chunks: 'all',
+            },
+            katex: {
+              test: /[\\/]node_modules[\\/](katex)[\\/]/,
+              name: 'katex-vendor',
+              chunks: 'async',
+            },
+            micromark: {
+              test: /[\\/]node_modules[\\/](micromark)[\\/]/,
+              name: 'micromark-vendor',
+              chunks: 'all',
+            },
+            prismjs: {
+              test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+              name: 'refractor-prismjs-vendor',
+              chunks: 'all',
+            },
+            runtime: {
+              test: /[\\/]node_modules[\\/](@babel[\\/]runtime)[\\/]/,
+              name: 'babel-runtime-vendor',
+              chunks: 'all',
+            },
+            parse5: {
+              test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+              name: 'parse5-vendor',
+              chunks: 'all',
+            },
           },
         },
-      },
-    };
-    if (env === 'production') {
+      };
       conf.output = { ...conf.output, publicPath: './' };
     }
   }
