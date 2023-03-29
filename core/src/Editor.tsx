58954905f2335b28235517583cb66cd1a1db7b69
UniqueUlysees <boriscool007@gmail.com>
2023-03-29 15:58:51 +0300

    editor: fix commands changes weren't reflected in editor state

--- core/src/Editor.tsx
+++ core/src/Editor.tsx
@@ -265,6 +265,8 @@ const InternalMDEditor = (
     () => height !== state.height && onHeightChange && onHeightChange(state.height, height, state),
     [height, onHeightChange, state],
   );
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   useMemo(() => commands !== state.commands && dispatch({ commands: commands }), [commands]);
 
   const textareaDomRef = useRef<HTMLDivElement>();
   const active = useRef<'text' | 'preview'>('preview');
