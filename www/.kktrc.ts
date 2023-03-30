d2607e3d4db7e4e3a4e60f8da556577f8995ffe5
jaywcjlove <398188662@qq.com>
2023-03-30 10:19:45 +0800

    chore(deps): update dependency tsbb to v4 #497

--- www/.kktrc.ts
+++ www/.kktrc.ts
@@ -1,18 +1,12 @@
-import path from 'path';
 import webpack, { Configuration } from 'webpack';
 import { LoaderConfOptions } from 'kkt';
 import lessModules from '@kkt/less-modules';
-import scopePluginOptions from '@kkt/scope-plugin-options';
 import { mdCodeModulesLoader } from 'markdown-react-code-preview-loader';
 import pkg from './package.json';
 
 export default (conf: Configuration, env: 'production' | 'development', options: LoaderConfOptions) => {
   conf = lessModules(conf, env, options);
   conf = mdCodeModulesLoader(conf);
-  conf = scopePluginOptions(conf, env, {
-    ...options,
-    allowedFiles: [path.resolve(process.cwd(), 'README.md')],
-  });
   // Get the project version.
   conf.plugins!.push(
     new webpack.DefinePlugin({
