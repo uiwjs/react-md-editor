354241cb2d5fbd6f1109a7d52e905d8bb32a2eed
jaywcjlove <398188662@qq.com>
2023-06-27 20:41:14 +0800

    fix(deps): update dependency react-code-preview-layout to v3 #486

--- www/src/Markdown.tsx
+++ www/src/Markdown.tsx
@@ -6,6 +6,10 @@ import rehypeIgnore from 'rehype-ignore';
 import data from '@uiw/react-md-editor/README.md';
 import { CodeComponent, ReactMarkdownNames } from 'react-markdown/lib/ast-to-react';
 
+const Preview = CodeLayout.Preview;
+const Code = CodeLayout.Code;
+const Toolbar = CodeLayout.Toolbar;
+
 const CodePreview: CodeComponent | ReactMarkdownNames = ({ inline, node, ...props }) => {
   const $dom = useRef<HTMLDivElement>(null);
   const { 'data-meta': meta, ...rest } = props as any;
@@ -29,14 +33,14 @@ const CodePreview: CodeComponent | ReactMarkdownNames = ({ inline, node, ...prop
     const code = data.data[metaId].value || '';
     const param = getURLParameters(meta);
     return (
-      <CodeLayout
-        ref={$dom}
-        style={{ marginBottom: 10 }}
-        toolbar={param.title || 'Example'}
-        code={<pre {...rest} />}
-        text={code}
-      >
-        <Child />
+      <CodeLayout>
+        <Preview>
+          <Child />
+        </Preview>
+        <Toolbar text={code}>{param.title || 'Example'}</Toolbar>
+        <Code>
+          <pre {...rest} />
+        </Code>
       </CodeLayout>
     );
   }
